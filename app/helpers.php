18d6d73f0f9f0f7369dfeaf240f43cc5b51f7b2c
ybb <1094307157@qq.com>
2021-08-04 10:21:49 +0000

    基础布局

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@
  * @datetime: 2021/8/4 5:41 下午
  */
 
-function test()
+function route_class()
 {
-    return 'test';
+    return str_replace('.', '-', Route::currentRouteName());
 }
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+       ©2010 - 2021 HJS 版權所有
+    </p>
+
+  </div>
+</footer>
