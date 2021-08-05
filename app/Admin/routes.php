eb904390e49741767857ac81067b7e5d8390e2d1
ybb <1094307157@qq.com>
2021-08-05 07:34:12 +0000

    管理后台-商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,5 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('home');
     $router->get('users', 'UsersController@index');
+    $router->get('products', 'ProductsController@index');
 });
