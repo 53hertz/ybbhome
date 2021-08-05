c4ac16ff3ae0b9692ec7c2ac9729468ed6509872
ybb <1094307157@qq.com>
2021-08-05 03:41:01 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserAddressRequest;
 use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,28 @@ class UserAddressesController extends Controller
 {
     public function index()
     {
-        $addresses = UserAddress::all();
+        $addresses = request()->user()->userAddresses;
         return view('user_addresses.index', compact('addresses'));
+    }
+
+    public function create()
+    {
+        $address = new UserAddress();
+        return view('user_addresses.create_and_edit', compact('address'));
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->userAddresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,9 @@ window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
