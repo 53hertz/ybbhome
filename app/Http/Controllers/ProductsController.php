4d51458a0a94692f7a823cb9e55a48a156b01bea
ybb <1094307157@qq.com>
2021-08-05 12:15:07 +0000

    用户界面-商品收藏

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -39,6 +39,31 @@ class ProductsController extends Controller
             throw new InvalidRequestException('该商品还未上架哦~');
         }
 
-        return view('products.show', compact('product'));
+        $favored = false;
+        if($user = request()->user()) {
+            $favored = boolval($user->favoriteProducts()->find($product->id));
+        }
+
+        return view('products.show', compact('product', 'favored'));
+    }
+
+    public function favor(Product $product, Request $request)
+    {
+        $user = $request->user();
+        if ($user->favoriteProducts()->find($product->id)) {
+            return [];
+        }
+
+        $user->favoriteProducts()->attach($product);
+
+        return [];
+    }
+
+    public function disfavor(Product $product, Request $request)
+    {
+        $user = $request->user();
+        $user->favoriteProducts()->detach($product);
+
+        return [];
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -16,7 +16,9 @@
               <div class="sales_and_reviews">
                 <div class="sold_count">累计销量 <span class="count">{{ $product->sold_count }}</span></div>
                 <div class="review_count">累计评价 <span class="count">{{ $product->review_count }}</span></div>
-                <div class="rating" title="评分 {{ $product->rating }}">评分 <span class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span></div>
+                <div class="rating" title="评分 {{ $product->rating }}">评分 <span
+                    class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span>
+                </div>
               </div>
               <div class="skus">
                 <label>选择</label>
@@ -34,20 +36,27 @@
                   @endforeach
                 </div>
               </div>
-              <div class="cart_amount"><label>数量</label><input type="text" class="form-control form-control-sm" value="1"><span>件</span><span class="stock"></span></div>
+              <div class="cart_amount"><label>数量</label><input type="text" class="form-control form-control-sm"
+                                                               value="1"><span>件</span><span class="stock"></span></div>
               <div class="buttons">
-                <button class="btn btn-success btn-favor">❤ 收藏</button>
-                <button class="btn btn-danger btn-add-to-cart">加入购物车</button>
+                @if($favored)
+                  <button class="btn btn-danger btn-disfavor">取消收藏</button>
+                @else
+                  <button class="btn btn-success btn-favor">❤ 收藏</button>
+                @endif
+                <button class="btn btn-warning btn-add-to-cart">加入购物车</button>
               </div>
             </div>
           </div>
           <div class="product-detail">
             <ul class="nav nav-tabs" role="tablist">
               <li class="nav-item">
-                <a class="nav-link active" href="#product-detail-tab" aria-controls="product-detail-tab" role="tab" data-toggle="tab" aria-selected="true">商品详情</a>
+                <a class="nav-link active" href="#product-detail-tab" aria-controls="product-detail-tab" role="tab"
+                   data-toggle="tab" aria-selected="true">商品详情</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#product-reviews-tab" aria-controls="product-reviews-tab" role="tab" data-toggle="tab" aria-selected="false">用户评价</a>
+                <a class="nav-link" href="#product-reviews-tab" aria-controls="product-reviews-tab" role="tab"
+                   data-toggle="tab" aria-selected="false">用户评价</a>
               </li>
             </ul>
             <div class="tab-content">
@@ -71,6 +80,38 @@
       $('.sku-btn').click(function () {
         $('.product-info .price span').text($(this).data('price'));
         $('.product-info .stock').text('库存：' + $(this).data('stock') + '件');
+      });
+
+      $('.btn-favor').click(function () {
+        // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
+        axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
+          .then(function () { // 请求成功会执行这个回调
+            swal('收藏成功', '', 'success')
+              .then(function () {
+                location.reload();
+              });
+          }, function (error) { // 请求失败会执行这个回调
+            // 如果返回码是 401 代表没登录
+            if (error.response && error.response.status === 401) {
+              swal('请先登录', '', 'error');
+            } else if (error.response && (error.response.data.msg || error.response.data.message)) {
+              // 其他有 msg 或者 message 字段的情况，将 msg 提示给用户
+              swal(error.response.data.msg ? error.response.data.msg : error.response.data.message, '', 'error');
+            } else {
+              // 其他情况应该是系统挂了
+              swal('系统错误', '', 'error');
+            }
+          });
+      });
+
+      $('.btn-disfavor').click(function () {
+        axios.delete('{{ route('products.disfavor', ['product' => $product->id]) }}')
+          .then(function () {
+            swal('已取消收藏', '', 'success')
+              .then(function () {
+                location.reload();
+              });
+          });
       });
     });
   </script>
