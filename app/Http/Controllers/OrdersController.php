6eb4ac829c29d54c9d4323b9967fc2a9b6613ee9
ybb <1094307157@qq.com>
2021-08-07 16:38:23 +0000

    确认收货

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -44,4 +44,21 @@ class OrdersController extends Controller
         return view('orders.show', compact('order'));
     }
 
+    public function received(Order $order)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        // 返回原页面
+        return $order;
+    }
+
 }
