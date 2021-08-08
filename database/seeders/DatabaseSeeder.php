734d3a4cf29c6849ae9ce696d85975e1a17d95fb
ybb <1094307157@qq.com>
2021-08-08 11:06:13 +0000

    导出管理后台数据

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            AdminTablesSeeder::class,
             UsersSeeder::class,
             UserAddressesSeeder::class,
             ProductsSeeder::class,
