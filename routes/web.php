8f2bf800c2c07a82c0447b81684ffbea86732b28
ybb <1094307157@qq.com>
2021-08-08 05:30:44 +0000

    用户界面-检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,8 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
 });
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
