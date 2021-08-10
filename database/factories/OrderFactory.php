7b4b0963b9534bc8da6087df299b625ad4bb26fb
ybb <1094307157@qq.com>
2021-08-10 05:55:19 +0000

    初始化秒杀模型

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -16,10 +16,7 @@ class OrderFactory extends Factory
         // 随机取一个用户
         $user = User::query()->inRandomOrder()->first();
         // 随机取一个该用户的地址
-<<<<<<< HEAD
-=======
         $address = $user->userAddresses()->inRandomOrder()->first();
->>>>>>> 引入 Horizon
         // 10% 的概率把订单标记为退款
         $refund = random_int(0, 10) < 1;
         // 随机生成发货状态
